ab933a3cd522737170a2b9c1d4dcd5bbb1b38209
Esmeralda Marín <65983756+EsmeraldaMarin@users.noreply.github.com>
2023-08-08 13:57:29 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
-let listo_btn = document.getElementById("lis")
-let limpiar_btn = document.getElementById("lim")
+let listo_btn_mcm = document.getElementById("lis1")
+let limpiar_btn_mcm = document.getElementById("lim1")
+
+let listo_btn_mcd = document.getElementById("lis2")
+let limpiar_btn_mcd = document.getElementById("lim2")
+
+
+
 let textarea = document.getElementById('numeros')
 let respuesta = document.getElementById('rta')
 
-listo_btn.addEventListener('click', ()=>{
+listo_btn_mcm?.addEventListener('click', ()=>{
     let numeros = textarea.value
     numeros = numeros.replace(/\s/g, '')
     numeros = numeros.split(",")
@@ -25,7 +31,6 @@ listo_btn.addEventListener('click', ()=>{
                     es_mult = false
                 }
             })
-                
             if (es_mult){
                 mcm = multiptope
             }
@@ -43,6 +48,56 @@ listo_btn.addEventListener('click', ()=>{
 
 
 })
-limpiar_btn.addEventListener('click', ()=>{
+
+listo_btn_mcd?.addEventListener('click', ()=>{
+    let numeros = textarea.value
+    numeros = numeros.replace(/\s/g, '')
+    numeros = numeros.split(",")
+
+    for(let i=0;i<numeros.length;i++){
+        numeros[i] = parseInt(numeros[i],10)
+    }
+
+    function calcular_mcd(numeros){
+        let mcd = 1
+        let menor = numeros[0]
+        let es_div_com = true
+        numeros.forEach(numero => {
+            if (numero < menor){
+                menor = numero
+            }
+        })
+
+        while (menor > 0){
+
+            for(let numero of numeros){
+                if (numero % menor != 0){
+                    es_div_com = false
+                    break
+                }
+            }
+            if (es_div_com){
+                mcd = menor
+                break
+            }
+            es_div_com = true
+            menor -= 1 
+        }
+        
+        return mcd
+    }
+    let rta = calcular_mcd(numeros)
+    respuesta.innerHTML = `
+    <div>La respuesta es:
+        <span class="r">${rta}</span>
+    </div>
+    `
+})
+
+
+limpiar_btn_mcm?.addEventListener('click', ()=>{
+    textarea.value = ""
+})
+limpiar_btn_mcd?.addEventListener('click', ()=>{
     textarea.value = ""
 })
